ddea461f2b16beb51cd50b65d12c036e0b0b2d31
Enes Aksoy <enesaksoy1168@gmail.com>
2021-02-14 14:10:12 +0300

    First commit.

---
+++ Teknasyon-EnesAksoy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teknasyon-EnesAksoy
+//
+//  Created by ENES AKSOY on 14.02.2021.
+//  Copyright © 2021 ENES AKSOY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

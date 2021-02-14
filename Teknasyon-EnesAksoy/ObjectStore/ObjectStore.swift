d5fd2dd87cfd384bb480b6cf64a51f461d38e6d7
Enes Aksoy <enesaksoy1168@gmail.com>
2021-02-14 19:09:35 +0300

    Added object store.

---
+++ Teknasyon-EnesAksoy/ObjectStore/ObjectStore.swift
@@ -0,0 +1,20 @@
+//
+//  ObjectStore.swift
+//  Teknasyon-EnesAksoy
+//
+//  Created by ENES AKSOY on 14.02.2021.
+//  Copyright © 2021 ENES AKSOY. All rights reserved.
+//
+
+import Foundation
+
+class ObjectStore {
+    
+    static let shared = ObjectStore()
+    
+    var nowPlayingData: ResponseModel?
+    var popularData: ResponseModel?
+    
+    var movieId: Int?
+    var imdbId: String?
+}
